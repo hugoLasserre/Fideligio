e1a2fead7ac285c39ada37214c9748569166af92
pauloducap <paulabeille3302@gmail.com>
2024-01-29 10:10:32 +0100

    docker install and working

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConsumersModule } from './consumers/consumers.module';
-import { DevelopersModule } from './developers/developers.module';
-import { dataSourceOptions } from 'db/data-source';
-import { RedisOptions } from 'ioredis';
-import * as redisStore from 'cache-manager-ioredis';
-import { CacheModule } from '@nestjs/cache-manager';
-import type { Cache } from 'cache-manager';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConsumersModule } from './consumers/consumers.module'
+import { DevelopersModule } from './developers/developers.module'
+import { dataSourceOptions } from 'db/data-source'
+import { RedisOptions } from 'ioredis'
+import * as redisStore from 'cache-manager-ioredis'
+import { CacheModule } from '@nestjs/cache-manager'
+import type { Cache } from 'cache-manager'
 
-export type CacheIORedis = Cache<ReturnType<typeof redisStore.create>>;
+export type CacheIORedis = Cache<ReturnType<typeof redisStore.create>>
 
 @Module({
   imports: [
@@ -19,7 +19,7 @@ export type CacheIORedis = Cache<ReturnType<typeof redisStore.create>>;
       store: redisStore,
 
       // Store-specific configuration:
-      host: 'localhost',
+      host: 'redis',
       port: 6379,
     }),
     TypeOrmModule.forRoot(dataSourceOptions),
