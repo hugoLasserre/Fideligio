a0437cbd0c56989146a2cd9c336c98ef08a7dd00
Hugo Lasserre <hugo.lasserrecab@gmail.com>
2024-01-29 09:57:42 +0100

    developper

--- src/consumers/dto/create-consumer.dto.ts
+++ src/consumers/dto/create-consumer.dto.ts
@@ -1,1 +1,14 @@
-export class CreateConsumerDto {}
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateConsumerDto {
+  
+    @IsNotEmpty()
+    @IsString()
+    email: string;
+  
+    @IsString()
+    password?: string;
+  
+    @IsNumber()
+    solde: number;
+}
