ffdbd637b1c5b63268dd2d1e0931bd2db717d41c
pauloducap <paulabeille3302@gmail.com>
2024-02-01 10:58:58 +0100

    rate limiter signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,19 +7,33 @@ import {
   Patch,
   Post,
   Request,
-  UseGuards
-} from '@nestjs/common';
-import { AuthGuard } from './auth.guard';
-import { AuthService } from './auth.service';
-import { ApiBearerAuth, ApiBody, ApiCreatedResponse, ApiForbiddenResponse, ApiNotFoundResponse, ApiOkResponse, ApiTags, ApiUnprocessableEntityResponse } from '@nestjs/swagger';
-import { Developer } from 'src/developers/entities/developer.entity';
-import { CreateDeveloperDto } from 'src/developers/dto/create-developer.dto';
+  UseGuards,
+} from '@nestjs/common'
+import { AuthGuard } from './auth.guard'
+import { AuthService } from './auth.service'
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnprocessableEntityResponse,
+} from '@nestjs/swagger'
+import { Developer } from 'src/developers/entities/developer.entity'
+import { CreateDeveloperDto } from 'src/developers/dto/create-developer.dto'
+import { Throttle } from '@nestjs/throttler'
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Throttle({ default: { limit: 1, ttl: 3000 } })
+
+
+  
   @Post('signup')
   @ApiCreatedResponse({ description: 'Created Succesfully' })
   @ApiUnprocessableEntityResponse({ description: 'Bad Request' })
@@ -29,7 +43,7 @@ export class AuthController {
     description: 'Json structure for developer object',
   })
   signIn(@Body() developer: Developer) {
-    return this.authService.signUp(developer);
+    return this.authService.signUp(developer)
   }
 
   @UseGuards(AuthGuard)
@@ -40,6 +54,6 @@ export class AuthController {
   @ApiForbiddenResponse({ description: 'Unauthorized Request' })
   @ApiUnprocessableEntityResponse({ description: 'Bad Request' })
   generateApiKey(@Request() req) {
-    return this.authService.generateApiKey(req.user.username);
+    return this.authService.generateApiKey(req.user.username)
   }
 }
